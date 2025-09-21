f557dc6ba4c2cd2f8f4765fe602267bdb33dde5e
Shashank Bharti <shashank_24a12res620@iitp.ac.in>
2025-09-21 22:38:43 +0530

    feat: initialize React Router project with Tailwind CSS and essential components
    - Created main application structure with App component, Header, Footer, and routing setup.
    - Implemented About, Contact, and Home components with basic layout and styling.
    - Set up index.js for component exports and main.jsx for routing configuration.
    - Configured Vite for development and build processes.
    - Included Tailwind CSS for styling throughout the application.

---
+++ React_Router/src/components/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home/Home";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Github from "./Github/Github";
+
+export {Home,About,Contact,Github}
