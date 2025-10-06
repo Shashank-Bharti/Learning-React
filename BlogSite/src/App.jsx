2696054c82b832659f8f899a7f7dbf1e934774c5
Shashank Bharti <shashank_24a12res620@iitp.ac.in>
2025-10-06 10:03:58 +0530

    feat: implement user authentication with Appwrite, add Header and Footer components, and configure Redux store

--- BlogSite/src/App.jsx
+++ BlogSite/src/App.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from "./appwrite/auth"
+import { useEffect } from 'react'
+import {login, logout} from "./features/authSlice"
+import { Header, Footer } from './components'
 
 function App() {
-  return (
-    <div className='bg-gray-400 h-screen text-white  w-full '>
-      Blog App with AppWrite
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  })
+  
+
+  
+  return !loading ? (
+    <div className='min-h-screen w-full flex flex-wrap bg-gray-400'>
+      <div className="w-full block">
+        <Header/>
+        test
+        <main>
+        {/* TODO:<Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
     </div>
-  )
+  ): (null)
 }
 
 export default App
