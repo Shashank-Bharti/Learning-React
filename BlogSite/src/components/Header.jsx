8a98a4f16c488737002f219e38db87bb62e709da
Shashank Bharti <shashank_24a12res620@iitp.ac.in>
2025-10-08 22:55:12 +0530

    feat: add LogoutBtn component and update Header to include authentication status

--- BlogSite/src/components/Header.jsx
+++ BlogSite/src/components/Header.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import{Container,Logo,LogoutBtn} from './index.js'
+import {useSelector} from 'react-redux'
+import {Link, useNavigate} from 'react-router-dom'
+
+
 
 function Header() {
+
+  const authStatus = useSelector((state)=> state.auth.status)
+
   return (
     <div>
       
--- BlogSite/src/components/index.js
+++ BlogSite/src/components/index.js
@@ -1,7 +1,14 @@
 import Header from './Header';
 import Footer from './Footer';
+import Container from './container';
+import LogoutBtn from './LogoutBtn';
+import Logo from './Logo';
+
 
 export {Header,
     Footer,
+    Container,
+    LogoutBtn,
+    Logo,
     
 }
